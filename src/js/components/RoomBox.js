287a8db7e19d0760ed7c2846237643127a15e954
kusera <kuserasera@gmail.com>
2015-09-08 14:04:12 -0400

    Implement RoomNode component
    - Add three sample RoomNodes to RoomBox
    - Remove hard-coded values to RoomHeader's rendering logic

--- src/js/components/RoomBox.js
+++ src/js/components/RoomBox.js
@@ -1,10 +1,14 @@
 var RoomHeader = require('./RoomHeader');
+var RoomNode = require('./RoomNode');
 
 var RoomBox = React.createClass({
     render : function () {
         return (
             <div className="room-box noSelect">
-                <RoomHeader/>
+                <RoomHeader title="Open Rooms"/>
+                <RoomNode url="Room1.db" name="Room1" />
+                <RoomNode url="Room2.db" name="Room2" />
+                <RoomNode url="Room2.db" name="Room3" />
             </div>
         );
     }
--- src/js/components/RoomHeader.js
+++ src/js/components/RoomHeader.js
@@ -2,7 +2,7 @@ var RoomHeader = React.createClass({
     render : function () {
         return(
             <div className="room-box-header">
-                <div className="room-box-title">Open Rooms</div>
+                <div className="room-box-title">{this.props.title}</div>
                 <div className="create-room-wrapper">
                      <div className="ion-plus-circled create-room-button room-hover"></div>
                 </div>
