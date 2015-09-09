a61cd47db9ccbaa98484882c9511665754eb197b
kusera <kuserasera@gmail.com>
2015-09-09 17:45:05 -0400

    Add date parsing and formatting for DateDivider

--- src/js/components/DateDivider.js
+++ src/js/components/DateDivider.js
@@ -1,9 +1,11 @@
 var DateDivider = React.createClass({
     render : function () {
+        var options = {month: 'long', day: 'numeric', year: 'numeric'};
+        var dateString = this.props.date.toLocaleDateString(undefined, options);
         return (
             <div className="date-divider">
                 <div className="divider-line"></div>
-                <div className="divider-label">{this.props.date}</div>
+                <div className="divider-label">{dateString}</div>
             </div>
         );
     }
