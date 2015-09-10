b595d61a48612815be8f1fb7b5dde60ae5f5911f
kusera <kuserasera@gmail.com>
2015-09-10 18:32:07 -0400

    Add server action handler
    - Changed VIEW-ACTION string to VIEW_ACTION

--- src/js/dispatcher/AppDispatcher.js
+++ src/js/dispatcher/AppDispatcher.js
@@ -3,7 +3,14 @@ var AppDispatcher = new Dispatcher();
 
 AppDispatcher.handleViewAction = function(action) {
     this.dispatch({
-        source: 'VIEW-ACTION',
+        source: 'VIEW_ACTION',
+        action: action
+    });
+};
+
+AppDispatcher.handleServerAction = function(action) {
+    this.dispatch({
+        source: 'SERVER_ACTION',
         action: action
     });
 };
