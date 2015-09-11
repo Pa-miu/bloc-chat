beb9c4141c062a4fc43c0fdc06be38a8086243d4
kusera <kuserasera@gmail.com>
2015-09-10 22:14:15 -0400

    Cleaned up code organization
    - Composite paths into single object
    - Made error message slightly more descriptive

--- src/js/api/ChatAPI.js
+++ src/js/api/ChatAPI.js
@@ -4,20 +4,25 @@ var AppConstants = require('../constants/AppConstants');
 var RoomActions = require('../actions/RoomActions');
 
 var firebaseRef = new Firebase('https://kusera-bloc-chat.firebaseio.com');
-var roomsPath = '/rooms/';
-var messagesPath = '/messages/';
-var membersPath = '/members/';
+var paths = {
+    rooms : '/rooms/',
+    messages : '/messages/',
+    members : '/members/'
+}
+
 
 var ChatAPI = {
     getRoom : function(){
-        firebaseRef.child(roomsPath).once('value', 
+        firebaseRef.child(paths.rooms).once('value', 
             function(data) {
                 RoomActions.roomFetched(data.val());
             },
             function(err) {
-                console.log('getRoom encountered' + err.getCode());
+                console.log('getRoom encountered an error: ' + err.getCode());
             }
         );
+    },
+    createRoom : function(){
     }
 };
 
