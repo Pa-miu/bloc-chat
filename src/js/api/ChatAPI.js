ff69efaaa6dff7392ef852c72c2581375ec46c7b
kusera <kuserasera@gmail.com>
2015-09-10 20:30:51 -0400

    Add API to handle firebase server interaction

--- src/js/api/ChatAPI.js
+++ src/js/api/ChatAPI.js
@@ -3,9 +3,22 @@ var AppDispatcher = require('../dispatcher/AppDispatcher');
 var AppConstants = require('../constants/AppConstants');
 var RoomActions = require('../actions/RoomActions');
 
-var firebaseRef = new Firebase('https://kusera-bloc-chat.firebaseio.com/');
+var firebaseRef = new Firebase('https://kusera-bloc-chat.firebaseio.com');
+var roomsPath = '/rooms/';
+var messagesPath = '/messages/';
+var membersPath = '/members/';
 
 var ChatAPI = {
+    getRoom : function(){
+        firebaseRef.child(roomsPath).once('value', 
+            function(data) {
+                RoomActions.roomFetched(data.val());
+            },
+            function(err) {
+                console.log('getRoom encountered' + err.getCode());
+            }
+        );
+    }
 };
 
 module.exports = ChatAPI;
