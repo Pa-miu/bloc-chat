e5c36b4719055fa19e457900ec0ad697126bf0d7
kusera <kuserasera@gmail.com>
2015-09-10 20:35:39 -0400

    Remove extraneous data attributes

--- src/js/components/RoomNode.js
+++ src/js/components/RoomNode.js
@@ -1,7 +1,7 @@
 var RoomNode = React.createClass({
     render : function() {
         return (
-            <div className="room-node room-hover" data-id={this.props.id} data-url={this.props.url}>
+            <div className="room-node room-hover">
                 {this.props.name}
             </div>
         );
