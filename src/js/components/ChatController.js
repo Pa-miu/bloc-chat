5d51d2468900a5ec8ed6d29d2320ae79260c426d
kusera <kuserasera@gmail.com>
2015-09-12 15:54:19 -0400

    Add skeleton for room creation modal

--- src/js/components/ChatController.js
+++ src/js/components/ChatController.js
@@ -1,12 +1,14 @@
 var RoomBox = require('./RoomBox');
 var MessageBox = require('./MessageBox');
+var CreateRoomModal = require('./CreateRooModal');
 
 var ChatController = React.createClass({
     render : function() {
         return (
-            <div>
+            <div>   
                 <RoomBox/>
                 <MessageBox/>
+                <CreateRoomModal/>
             </div>
         );
     }
---
+++ src/js/components/CreateRoomModal.js
@@ -0,0 +1,36 @@
+var CreateRoomModal = React.createClass({
+    _hideMyself : function() {
+        this.props.toggle(this);
+    },
+    
+    _handleSubmit : function() {
+        var newRoom = {
+            name = "";
+        };
+        this.props.create(newRoom);
+    },
+    
+    render : function(){
+        return (
+            <div className="modal-background">
+                <form className="modal-form">
+                    <div className="modal-header">
+                        "Create a New Room"
+                    </div>
+                    <div className="modal-body">
+                        <label className="modal-label">
+                            "Room name:"
+                            <input className="modal-input"></input>
+                        </label>
+                    </div>
+                    <div className="modal-footer">
+                        <button className="modal-cancel">Cancel</button>
+                        <button className="modal-submit">Submit</button>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+});
+
+module.exports = CreateRoomModal;
