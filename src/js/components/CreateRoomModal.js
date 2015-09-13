c5c9150a9cef3f91599f77f869cc0e77e151dbe3
kusera <kuserasera@gmail.com>
2015-09-12 20:22:34 -0400

    Finish basic modal styling
    - Also removed some whitespace from room.css

--- src/js/components/CreateRoomModal.js
+++ src/js/components/CreateRoomModal.js
@@ -1,31 +1,33 @@
 var CreateRoomModal = React.createClass({
-    _hideMyself : function() {
-        this.props.toggle(this);
+    _handleClose : function() {
+        this.props.close(this);
     },
     
     _handleSubmit : function() {
         var newRoom = {
             name : ""
         };
-        this.props.create(newRoom);
+        this.props.submit(newRoom);
     },
     
     render : function(){
         return (
             <div className="modal-dialog">
+                <div className="modal-dim" onClick={this._handleClose}></div>
                 <form className="modal-form">
                     <div className="modal-header">
                         Create a New Room
                     </div>
                     <div className="modal-body">
-                        <label className="modal-label">
-                            Room name:
-                            <input className="modal-input"></input>
-                        </label>
+                        <p>Create a public room on the server every user can see and join.</p>
+                        <div className="modal-entry-group">
+                            <label className="modal-label" for="room-name-input">Name</label>
+                            <input className="modal-input" id="room-name-input"></input>
+                        </div>
                     </div>
                     <div className="modal-footer">
-                        <button className="modal-cancel">Cancel</button>
-                        <button className="modal-submit">Submit</button>
+                        <div className="modal-button red" onClick={this._handleClose}>Cancel</div>
+                        <div className="modal-button green" onClick={this._handleSubmit}>Submit</div>
                     </div>
                 </form>
             </div>
