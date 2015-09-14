9edc8dc720a2867d20987db749441c719b2c125b
kusera <kuserasera@gmail.com>
2015-09-14 02:31:14 -0400

    Move message converstion to MessageBox

--- src/js/components/MessageList.js
+++ src/js/components/MessageList.js
@@ -1,36 +1,11 @@
 var React = require('React');
 
-var MessageInstance = require('./MessageInstance');
-var DateDivider = require('./DateDivider');
-
-var MessageList = React.createClass({
-    objectToMessage : function(object) {
-        var newMessage;
-        if(object.hasOwnProperty('sender')){
-            newMessage = (
-                <MessageInstance
-                    key={object.id}
-                    iconURL={object.iconURL}
-                    sender={object.sender}
-                    timestamp={object.timestamp}
-                    content={object.content}
-                />
-            );
-        }
-        else if (object.hasOwnProperty('date')){
-            newMessage = (
-                <DateDivider key={object.id} date={object.date}/>
-            );
-        }
-        return newMessage;
-    },
-        
+var MessageList = React.createClass({    
     render : function () {
-        var messages = this.props.messages.map(this.objectToMessage);
         return (
             <div className="message-list-wrapper">
                 <div className="message-list">
-                    {messages}
+                    {this.props.messages}
                 </div>
                 <div className="message-scroll"></div>
             </div>
