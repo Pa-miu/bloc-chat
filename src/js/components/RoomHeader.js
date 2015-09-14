50649ca6e2181f306dce57a46a234f10eb163e79
kusera <kuserasera@gmail.com>
2015-09-13 21:00:40 -0400

    Add buttons for closing rooms
    - Tightened up the class names for RoomHeader and RoomNode
    - RoomHeader and RoomNode now use a similar layout and styles
    - Styling in room.css updated with same techniques used in modal.css
    - Added max width to RoomHeader and RoomNode
    - Reorganized room.css

--- src/js/components/RoomHeader.js
+++ src/js/components/RoomHeader.js
@@ -4,8 +4,8 @@ var RoomHeader = React.createClass({
     render : function () {
         return(
             <div className="room-box-header">
-                <div className="room-box-title">{this.props.title}</div>
-                <div className="create-room-wrapper">
+                <div className="room-header-label">{this.props.title}</div>
+                <div className="room-button-wrapper create-room-position">
                     <div className="ion-plus-circled create-room-button room-hover" onClick={this.props.toggle}></div>
                 </div>
             </div>
--- src/js/components/RoomNode.js
+++ src/js/components/RoomNode.js
@@ -4,7 +4,10 @@ var RoomNode = React.createClass({
     render : function() {
         return (
             <div className="room-node room-hover">
-                {this.props.name}
+                <div className="room-node-label">{this.props.name}</div>
+                <div className="room-button-wrapper delete-room-position">
+                    <div className="ion-close delete-room-button room-hover" onClick={this.props.delete}></div>
+                </div>
             </div>
         );
     }
