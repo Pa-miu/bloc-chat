Pa-miu/bloc-chat

commit 4dcf8871073d852aef3400bd78e1a3424cabbdc9

Remove room name transformation for now

src/js/api/ChatAPI.js
@@ -11,9 +11,7 @@ var paths = {
 };
 
 var ChatAPI = {
-    createRoom : function(room){
-        var name = room.name.toLowerCase();
-        
+    createRoom : function(room){   
         firebaseRef.child(paths.rooms + name).once('value', 
             function(data) {
                 if (!data.exists()) {

commit 0806ab7a942d3e511175445646974cab0d749106

Improve functionality of createRoom and deleteRoom
- Create room and delete room now properly create the firebase objects
  necessary for storing messages and memberlist
- Also included a dummy message for new rooms
- Fixed broken variables from 4dcf887
- Improved argument names

src/js/api/ChatAPI.js
@@ -10,24 +10,46 @@ var paths = {
     members : '/members/'
 };
 
+var messagesTemplate = function (name){
+    return {
+        "0greeting" : {
+            "sender" : "server",
+            "timestamp" : new Date().toUTCString(),
+            "iconURL" : "",
+            "content" : "This is your room!"
+        }
+    }
+}
+
 var ChatAPI = {
-    createRoom : function(room){   
+    createRoom : function(newRoom){
+        var name = newRoom.name;
         firebaseRef.child(paths.rooms + name).once('value', 
             function(data) {
-                if (!data.exists()) {
-                    firebaseRef.child(paths.rooms + name).set(room);
+                if (!data.exists()){ 
+                    firebaseRef.child(paths.rooms + name).set(newRoom);
+                    firebaseRef.child(paths.messages + name).set(messagesTemplate());
+                    firebaseRef.child(paths.members + name).set(name);
                 }
-            }   
+            },
+            function(error) {
+                console.log('ChatAPI.createRoom() encountered an error: ' + error.getCode());
+            }
         );
     },
     
-    deleteRoom : function(name){
-        firebaseRef.child(paths.rooms + name).once('value',
+    deleteRoom : function(roomName){
+        firebaseRef.child(paths.rooms + roomName).once('value',
             function(data) {
-                if (data.exists()) {
-                    firebaseRef.child(paths.rooms + name).set(null);
+                if (data.exists()){ 
+                    firebaseRef.child(paths.rooms + roomName).set(null); 
+                    firebaseRef.child(paths.messages + roomName).set(null);
+                    firebaseRef.child(paths.members + roomName).set(null);
                 }
-            }  
+            },
+            function(error) {
+                console.log('ChatAPI.deleteRoom() encountered an error: ' + error.getCode());
+            }
         );
     },
     