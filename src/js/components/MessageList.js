fdc7f573a9667d2dcd44edbdf33867690654d2ff
kusera <kuserasera@gmail.com>
2015-09-16 00:40:56 -0400

    Add auto scrolling to MessageList

--- src/js/components/MessageList.js
+++ src/js/components/MessageList.js
@@ -1,10 +1,25 @@
 var React = require('react');
 
-var MessageList = React.createClass({    
+var SCROLL_ZONE = 100;
+
+var MessageList = React.createClass({  
+    componentDidMount : function () {
+        listRef = React.findDOMNode(this.refs.messageList);
+        listRef.scrollTop = listRef.scrollHeight;
+    },
+    
+    componentDidUpdate : function () {
+        listRef = React.findDOMNode(this.refs.messageList);
+        
+        if (listRef.scrollHeight - (listRef.scrollTop + listRef.clientHeight) > SCROLL_ZONE) {
+            listRef.scrollTop = listRef.scrollHeight;
+        }
+    },
+    
     render : function () {
         return (
-            <div className="message-list-wrapper">
-                <div className="message-list">
+            <div className="message-list-wrapper" >
+                <div className="message-list" ref="messageList">
                     {this.props.messages}
                 </div>
                 <div className="message-scroll"></div>
