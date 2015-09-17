01a04cf1256cd906dddde0819291c5278fdb0e7b
kusera <kuserasera@gmail.com>
2015-09-17 01:58:34 -0400

    Change pre-render setup
    - Dummy user template serves as the "default" user. Later on will be
      dynamically generated by querying users on the server.
    - Initial message fetch will target the user's hypothetical 'last
      visited room'

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,13 @@ var React = require('react');
 var ChatAPI = require('./api/ChatAPI');
 var ChatController = require('./components/ChatController');
 
+var defaultUser = {
+    username : 'Guest',
+    currentRoom : 'dummy'
+};
+
+ChatAPI.setUser(defaultUser);
 ChatAPI.getRoomList();
+ChatAPI.getMessages(defaultUser.currentRoom);
 
 React.render(<ChatController />, document.getElementById('app'));
